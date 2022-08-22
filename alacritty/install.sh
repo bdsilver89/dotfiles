a3d55c93649a857230096db783b5c828ae6017a8
bdsilver89 <github@briansilver.net>
2022-08-22 10:09:44 -0400

    updated alacritty install with WSL instructions until a fix is found. Moved nvim spacing config

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -2,4 +2,14 @@
 
 source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 
+do_install() {
+    if [[ $WSL == 1 ]]; then
+        fail "WSL not yet supported. Please just copy alacritty.yml into Windows \"%APPDATA%\\alacritty\\alacritty.yml\""
+
+    else
+        mkdir -p "$dst_conf" 2>/dev/null
+        link_file "$src_conf/alacritty.yml" "$dst_conf/$file"
+    fi
+}
+
 install
--- nvim/lua/bdsilver89/base.lua
+++ nvim/lua/bdsilver89/base.lua
@@ -14,13 +14,14 @@ vim.opt.showcmd = true
 vim.opt.backup = false
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
-vim.opt.expandtab = true
 vim.opt.scrolloff = 10
 vim.opt.shell = 'zsh'
 vim.opt.backupskip = { '/tmp/*', '/private/tmp' }
 vim.opt.ignorecase = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop=4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
 vim.opt.wrap = false
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' }
