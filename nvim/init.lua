62de84f84967a9e9d3a7b7e31464d146ea0f82b2
bdsilver89 <github@briansilver.net>
2022-08-23 23:32:43 -0400

    Switched out for lighter config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,17 @@
-if not vim.g.vscode then
-	require("core")
-end
+require('bdsilver89.base')
+require('bdsilver89.highlights')
+require('bdsilver89.maps')
+require('bdsilver89.plugins')
+
+-- local has = function(x)
+--   return vim.fn.has(x) == 1
+-- end
+-- local is_mac = has "macunix"
+-- local is_win = has "win32"
+
+-- if is_mac then
+--   require('craftzdog.macos')
+-- end
+-- if is_win then
+--   require('craftzdog.windows')
+-- end
--- nvim/install.sh
+++ nvim/install.sh
@@ -4,8 +4,15 @@ source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 
 do_install() {
   mkdir -p "$dst_conf" 2>/dev/null
+
+  link_file "$src_conf/after" "$dst_conf/after"
+  link_file "$src_conf/plugin" "$dst_conf/plugin"
   link_file "$src_conf/lua" "$dst_conf/lua"
   link_file "$src_conf/init.lua" "$dst_conf/init.lua"
+  # link_file "$src_conf/coc-settings.json" "$dst_conf/coc-settings.json"
+
+  # info "Installing nvim's plugins..."
+  # nvim --headless +PackerInstall +qall
 }
 
 install
