9f917c2e04ddd30ef85867ec7db8b8a64d9358f7
Brian Silver <brian.silver@bgcpartners.com>
2022-08-24 14:14:39 -0400

    Changed nvim split cursor movement

--- nvim/lua/bdsilver89/maps.lua
+++ nvim/lua/bdsilver89/maps.lua
@@ -18,18 +18,28 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- New tab
 keymap.set('n', 'te', ':tabedit')
 keymap.set('n', 'tc', ':tabclose')
+
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
+
+-- Move windows with hjkl
 keymap.set('', 'sh', '<C-w>h')
 keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
 keymap.set('', 'sl', '<C-w>l')
 
--- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+-- Move windows with extended arrows
+-- keymap.set('', '', '<C-w>h')
+-- keymap.set('', '', '<C-w>j')
+-- keymap.set('', '', '<C-w>k')
+-- keymap.set('', '', '<C-w>l')
+
+-- Resize window with ctrl-arrows
+keymap.set('n', '<C-w><C-left>', '<C-w><')
+keymap.set('n', '<C-w><C-right>', '<C-w>>')
+keymap.set('n', '<C-w><C-up>', '<C-w>+')
+keymap.set('n', '<C-w><C-down>', '<C-w>-')
