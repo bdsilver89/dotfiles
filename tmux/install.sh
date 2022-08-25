d14d5ddcffddf964f1b0477c0ac29fb2c01a2fca
bdsilver89 <github@briansilver.net>
2022-08-24 20:43:20 -0400

    fixed bad tmux tpm clone path

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,8 +8,7 @@ do_install() {
 
   # install tpm for packages
   info "Installing tmux plguins..."
-  rm -rf "$dst_conf/plugins/tpm"
-  git clone --depth 1 https://github.com/tmux-plugins/tpm "$dst_conf/plugins/tpm" 2>/dev/null
+  git clone --depth 1 https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm" 2>/dev/null
 
   link_file "$src_conf/tmux.conf" "$dst_conf/tmux.conf"
   link_file "$src_conf/tmux.conf" "${HOME}/.tmux.conf"
