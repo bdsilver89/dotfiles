830861f1fe5937c797b80e3e99d3ab57dc79c446
Brian Silver <github@briansilver.net>
2023-08-20 23:04:09 -0400

    fixed annoying bug with toggleterm mapping in insert mode

--- nvim/.config/nvim/lua/plugins/base/editor.lua
+++ nvim/.config/nvim/lua/plugins/base/editor.lua
@@ -571,13 +571,13 @@ return {
   {
     "akinsho/toggleterm.nvim",
     keys = {
-      { "<leader>ct", desc = "Toggleterm", mode = "n" },
+      { "<leader>ct", "<cmd>ToggleTerm<cr>", desc = "Toggleterm", mode = "n" },
     },
     cmd = { "ToggleTerm", "TermExec" },
     opts = {
       size = 20,
       hide_numbers = true,
-      open_mapping = [[<leader>ct]],
+      -- open_mapping = [[<leader>ct]],
       shade_filetypes = {},
       shade_terminals = false,
       shading_factor = 0.3,
