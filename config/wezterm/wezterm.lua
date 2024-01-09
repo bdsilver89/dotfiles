ff86e65acb79770863465312d728fef033ec1170
Brian Silver <github@briansilver.net>
2024-01-09 15:19:49 -0500

    change wezterm theme to catppuccin

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 return {
-	-- color_scheme = "Catppuccin Mocha",
-	color_scheme = "Tokyo Night Moon",
+	color_scheme = "Catppuccin Mocha",
+	-- color_scheme = "Tokyo Night Moon",
 	enable_tab_bar = false,
 	font_size = 16.0,
 	macos_window_background_blur = 19,
