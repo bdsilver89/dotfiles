d3eccee1cf1b540e974d4b59f532af5534efc1c7
Brian Silver <github@briansilver.net>
2024-03-29 00:22:16 -0400

    started powershell script for bootstrapping on windows

---
+++ bootstrap.ps1
@@ -0,0 +1,15 @@
+# NOTE: look at this repo for help initializing windows powershell
+# https://github.com/JMOrbegoso/Dotfiles-for-Windows-11/tree/main
+
+
+Write-Host "Updating" -ForegroundColor "Magenta"
+Write-Host "=============================="
+git pull origin main
+
+Write-Host "Setting up symlinks" -ForegroundColor "Magenta"
+Write-Host "=============================="
+# alacritty
+# nvim
+# TODO: powershell profile
+
+Write-Host "Done" -ForegroundColor "Green"
