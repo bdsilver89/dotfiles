99201800b42e6e418d5ea59841e7125d02dd02fb
Brian Silver <github@briansilver.net>
2024-03-29 12:57:47 -0400

    added functionality to windows powershell bootstrap script, added powershell profile and functions/aliases to help dev workflow

---
+++ WindowsPowerShell/aliases.ps1
@@ -0,0 +1,20 @@
+
+# mimic chocolatey's refresh env
+Set-Alias refreshenv Update-SessionEnvironment
+
+Set-Alias cl clear;
+
+Set-Alias c bat;
+
+if (Get-Command zoxide -ErrorAction SilentlyContinue | Test-Path) {
+  Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
+if (Get-Command eza -ErrorAction SilentlyContinue | Test-Path) {
+  ${function:ls} = { eza --icons --git}
+  ${function:l} = { eza -l --icons --git}
+  ${function:lt} = { eza --tree --level=2 --long --icons --git}
+}
+
+
+#
+# configure visual studio
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,15 +1,91 @@
+param(
+  [switch]$update = $true,
+  [switch]$winget = $true,
+  [switch]$symlinks = $true
+)
+
 # NOTE: look at this repo for help initializing windows powershell
 # https://github.com/JMOrbegoso/Dotfiles-for-Windows-11/tree/main
 
+$DotfilesDir = Join-Path -Path $HOME -ChildPath "dotfiles";
+$ProfileDir = Split-Path -parent $profile;
 
-Write-Host "Updating" -ForegroundColor "Magenta"
-Write-Host "=============================="
-git pull origin main
 
-Write-Host "Setting up symlinks" -ForegroundColor "Magenta"
-Write-Host "=============================="
-# alacritty
-# nvim
-# TODO: powershell profile
+function Title {
+  param([string]$msg)
+  Write-Host "$msg" -ForegroundColor "Magenta";
+  Write-Host "==============================";
+}
 
-Write-Host "Done" -ForegroundColor "Green"
+function Error {
+  param([string]$msg)
+  Write-Host "$msg" -ForegroundColor "Red";
+}
+
+function Warning {
+  param([string]$msg)
+  Write-Host "$msg" -ForegroundColor "Yellow";
+}
+
+function Info {
+  param([string]$msg)
+  Write-Host "$msg";
+}
+
+function Success {
+  param([string]$msg)
+  Write-Host "$msg" -ForegroundColor "Green";
+}
+
+function ReloadPath {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+function SetupSymlink {
+  param([string]$source,[string]$target)
+
+  if (Test-Path -Path $target) {
+    Warning "$target path already exists, skipping"
+  }
+  else {
+    New-Item -Path $target -ItemType SymbolicLink -Value $source;
+  }
+}
+
+if ($update) {
+  Title "Updating dotfiles repository";
+  git pull origin main;
+}
+
+if ($winget) {
+  Title "Installing winget programs";
+
+  # terminal environment
+  # winget install Cygwin.Cygwin;
+  winget install Alacritty.Alacritty
+
+  # terminal tools
+  winget install ajeetsouza.zoxide;
+  winget install eza-community.eza;
+  winget install sharkdp.bat;
+
+  # editors
+  winget install Microsoft.VisualStudioCode;
+  winget install Neovim.Neovim;
+
+  # development tools
+  # winget install Docker.DockerDesktop;
+
+  ReloadPath;
+}
+
+if ($symlinks) {
+  Title "Setting up symlinks";
+
+  $AlacrittyDir = Join-Path -Path $HOME -ChildPath "AppData/Roaming/alacritty";
+
+  SetupSymlink -source $DotfilesDir/WindowsPowerShell -target $ProfileDir;
+  SetupSymlink -source $DotfilesDir/config/alacritty -target $AlacrittyDir;
+}
+
+Success "Done";
