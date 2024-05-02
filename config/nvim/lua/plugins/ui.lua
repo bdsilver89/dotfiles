295c4320a1fc1b111ed89ae41567a561598f99d5
Brian Silver <github@briansilver.net>
2024-05-02 09:41:15 -0400

    nvim lualine updates

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -9,9 +9,19 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
+      -- disable icons in lualine if needed
+      opts.options.icons_enabled = vim.g.enable_icons
+
       -- prefer these separators over defaults
       opts.options.component_separators = "|"
       opts.options.section_separators = ""
+
+      -- add fileencoding
+      vim.list_extend(opts.sections.lualine_x, { "encoding" })
+
+      -- add fileformat to track line endings
+      vim.list_extend(opts.sections.lualine_x, { "fileformat" })
+
       return opts
     end,
   },
