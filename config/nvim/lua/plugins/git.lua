43d919ace555f88e9a92902aa2bfee709a716738
Brian Silver <github@briansilver.net>
2024-05-16 17:33:50 -0400

    added neogit back to nvim config

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {},
+  },
+
+  {
     "tpope/vim-fugitive",
     lazy = false,
   },
