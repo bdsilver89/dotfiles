9ff14b879cd65205b001009b8d8394cc93636242
Brian Silver <github@briansilver.net>
2024-05-20 00:44:01 -0400

    cleaning up bash/zsh setups

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,66 @@
 
 DOTFILES_DIR=$(cd -- "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-# use some of the submodules bundled with dotfiles
-source $DOTFILES_DIR/bash/utils.sh
-source $DOTFILES_DIR/bash/logging.sh
+# ----------------------------------------------------------------------------
+# Logging
+# ----------------------------------------------------------------------------
+LOG_VERBOSITY=${LOG_VERBOSITY:-2}
+LOG_DATE=${LOG_DATE:-true}
+LOG_CONSOLE=${LOG_CONSOLE:-false}
+LOG_FORCE_COLORS=${LOG_FORCE_COLORS:-false}
+
+LOG_BG=${1:-1}
+LOG_COLOR_DEFAULT="\e[0;39m"
+LOG_COLOR_GREY="\e[$BG;30m"
+LOG_COLOR_RED="\e[$BG;31m"
+LOG_COLOR_GREEN="\e[$BG;32m"
+LOG_COLOR_YELLOW="\e[$BG;33m"
+LOG_COLOR_BLUE="\e[$BG;34m"
+LOG_COLOR_PURPLE="\e[$BG;35m"
+LOG_COLOR_CYAN="\e[$BG;36m"
+
+log() {
+	if [ -t 1 ]; then
+		printf "${@}"
+		printf "\n"
+	fi
+}
+
+log_err() {
+	log "${LOG_COLOR_RED}$*${LOG_COLORDEFAULT}"
+}
+
+log_warn() {
+	log "${LOG_COLOR_YELLOW}$*${LOG_COLOR_DEFAULT}"
+}
+
+log_info() {
+	log "${LOG_COLOR_GREEN}$*${LOG_COLOR_DEFAULT}"
+}
+
+log_status() {
+	log "${LOG_COLOR_BLUE}$*${LOG_COLOR_DEFAULT}"
+}
+
+log_success() {
+	log "${LOG_COLOR_GREEN}\nSuccess${LOG_COLOR_DEFAULT}!"
+}
+
+log_failure() {
+	log "${LOG_COLOR_RED}\nFailure${LOG_COLOR_DEFAULT}!"
+}
+
+log_and_die() {
+	local rc=$?
+	local msg=${*:-"unknown error"}
+	if [ $rc -ne 0 ]; then
+		log_err "Aborting - command failed with rc=$rc - $msg"
+		exit $rc
+	else
+		log_err "Aborting - $msg"
+		exit $rc
+	fi
+}
 
 title() {
 	log "\n${LOG_COLOR_PURPLE}$*${LOG_COLOR_DEFAULT}"
@@ -30,6 +87,74 @@ status() {
 
 success() {
 	log_info "$*"
+}
+
+# -----------------------------------------------------------------------------
+# OS/platform
+# -----------------------------------------------------------------------------
+os() {
+	local name=$(uname -s)
+	if [[ $name =~ MINGW ]] || [[ $os =~ MSYS ]]; then
+		echo windows
+	elif [[ $name =~ FreeBSD ]]; then
+		echo bsd
+	elif [[ $name =~ Darwin ]]; then
+		echo darwin
+	else
+		echo $name
+	fi
+}
+
+platform() {
+	if [ -f /etc/debian_version ]; then
+		echo debian
+	elif [ -f /etc/os-release ]; then
+		local id=$(egrep ^ID= /etc/os-release | sed 's/.*=//; s/"//g')
+		local version=$(egrep ^VERSION_ID= /etc/os-release | sed 's/.*=//; s/"//g' | cut -d. -f1)
+		echo $id$version
+	elif [[ $(os) == "darwin" ]]; then
+		local id="macos"
+		local version=$(sw_vers | egrep ^ProductVersion | sed 's/.*://; s/ //g' | cut -d. -f1)
+		echo $id$version
+	else
+		echo win64
+	fi
+}
+
+is_windows() {
+  [ ${$(platform):0:3} = win ]
+}
+
+is_wsl() {
+  [[ -n $WSL_DISTO_NAME ]]
+}
+
+is_linux() {
+  [ $(os) = linux ]
+}
+
+is_debian() {
+  [ $(platform) = debian ]
+}
+
+is_darwin() {
+  [ $(os) = darwin ]
+}
+
+
+# -----------------------------------------------------------------------------
+# date/time utils
+# -----------------------------------------------------------------------------
+isodate() {
+  date +%Y-%m-%dT%H-%M:%Sz
+}
+
+isodate_utc() {
+  date -u +%Y-%m-%dT%H-%M:%SZ
+}
+
+unixstamp() {
+  date +%s
 }
 
 link_file() {
--- zsh/env.zsh
+++
@@ -1,3 +0,0 @@
-export EDITOR='nvim'
-
-export PATH="$HOME/.local/bin:$PATH"
