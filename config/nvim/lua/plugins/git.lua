c9558aa2d2216098217af06c9b82bcae21d63dbb
Brian Silver <github@briansilver.net>
2024-06-02 23:08:41 -0400

    lazyvim updates

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -3,6 +3,7 @@ return {
   {
     "NeogitOrg/neogit",
     name = "Neogit",
+    enabled = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
@@ -15,5 +16,31 @@ return {
       { "<leader>gp", "<cmd>Neogit push<cr>", desc = "Push" },
     },
     opts = {},
+  },
+
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+
+  {
+    "SuperBo/fugit2.nvim",
+    enabled = false,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+      {
+        "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
+        dependencies = { "stevearc/dressing.nvim" },
+      },
+    },
+    cmd = { "Fugit2", "Fugit2Blame", "Fugit2Diff", "Fugit2Graph" },
+    keys = {
+      { "<leader>gm", mode = "n", "<cmd>Fugit2<cr>" },
+    },
+    opts = {
+      width = 70,
+    },
   },
 }
