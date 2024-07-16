f7cbe5c1060088e64d697996e315bee8362be000
Brian Silver <github@briansilver.net>
2024-07-16 13:22:12 -0400

    updates to vscode nvim config

--- config/nvim-custom/lua/plugins/vscode.lua
+++ config/nvim-custom/lua/plugins/vscode.lua
@@ -5,6 +5,7 @@ end
 local enabled = {
   "lazy.nvim",
   "nvim-treesitter",
+  "nvim-treesitter-textobjects",
 }
 
 local Config = require("lazy.core.config")
@@ -13,6 +14,19 @@ Config.options.change_detection.enabled = false
 Config.options.defaults.cond = function(plugin)
   return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
 end
+
+-- stylua: ignore start
+vim.keymap.set("n", "<leader><leader>", "<cmd>Find<cr>")
+vim.keymap.set("n", "<leader>/", function() require("vscode-neovim").call("workbench.action.findInFiles") end)
+vim.keymap.set("n", "<leader>ss", function() require("vscode-neovim").call("workbench.action.gotoSymbol") end)
+-- stylua: ignore end
+
+vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<c-k>", "<c-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<c-l>", "<c-w>l", { desc = "Go to right window", remap = true })
+
+vim.notify = require("vscode-neovim").notify
 
 return {
   {
