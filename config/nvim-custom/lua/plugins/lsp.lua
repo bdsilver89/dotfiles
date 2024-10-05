2bc66557f01d05d9a91cb856393db7e4e066578d
Brian Silver <github@briansilver.net>
2024-10-05 11:33:28 -0400

    fix lsp renamer window

--- config/nvim-custom/lua/plugins/lsp.lua
+++ config/nvim-custom/lua/plugins/lsp.lua
@@ -48,46 +48,46 @@ return {
           -- map("gG", vim.lsp.buf.workspace_symbol, "Search workspace symbols")
           -- map("gI", function() require("telescope.builtin").lsp_implementations() end, "Goto implementation")
           -- map("gy", function() require("telescope.builtin").lsp_type_definitions() end, "Goto type defintion")
-          map("<leader>cr", vim.lsp.buf.rename, "Rename")
+          -- map("<leader>cr", vim.lsp.buf.rename, "Rename")
           map("<leader>ca", vim.lsp.buf.code_action, "Code action")
           map("<leader>cd", vim.diagnostic.open_float, "Line diagnostics")
           map("<leader>cl", "<cmd>LspInfo<cr>", "Info")
           -- stylua: ignore end
 
-          -- map("<leader>cr", function()
-          --   local curr_name = vim.fn.expand("<cword>" .. " ")
-          --   local win = require("plenary.popup").create(curr_name, {
-          --     title = "Rename",
-          --     style = "minimal",
-          --     borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-          --     relative = "cursor",
-          --     borderhighlight = "RenamerBorder",
-          --     titlehighlight = "RenamerTitle",
-          --     focusable = true,
-          --     width = 25,
-          --     height = 1,
-          --     line = "cursor+2",
-          --     col = "cursor-1",
-          --   })
-          --
-          --   vim.cmd("normal A")
-          --   vim.cmd("startinsert")
-          --
-          --   vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>q<cr>", { buffer = 0 })
-          --
-          --   vim.keymap.set({ "i", "n" }, "<cr>", function()
-          --     local new_name = vim.trim(vim.fn.getline("."))
-          --     vim.api.nvim_win_close(win, true)
-          --
-          --     if #new_name > 0 and new_name ~= curr_name then
-          --       local params = vim.lsp.util.make_position_params()
-          --       params.newName = new_name
-          --
-          --       vim.lsp.buf_request(0, "textDocument/rename", params)
-          --     end
-          --     vim.cmd.stopinsert()
-          --   end, { buffer = 0 })
-          -- end, "Rename")
+          map("<leader>cr", function()
+            local curr_name = vim.fn.expand("<cword>") .. " "
+            local win = require("plenary.popup").create(curr_name, {
+              title = "Rename",
+              style = "minimal",
+              borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+              relative = "cursor",
+              -- borderhighlight = "RenameBorder",
+              -- titlehighlight = "RenameTitle",
+              focusable = true,
+              width = 25,
+              height = 1,
+              line = "cursor+2",
+              col = "cursor-1",
+            })
+
+            vim.cmd("normal A")
+            vim.cmd("startinsert")
+
+            vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>q<cr>", { buffer = 0 })
+
+            vim.keymap.set({ "i", "n" }, "<cr>", function()
+              local new_name = vim.trim(vim.fn.getline("."))
+              vim.api.nvim_win_close(win, true)
+
+              if #new_name > 0 and new_name ~= curr_name then
+                local params = vim.lsp.util.make_position_params()
+                params.newName = new_name
+
+                vim.lsp.buf_request(0, "textDocument/rename", params)
+              end
+              vim.cmd.stopinsert()
+            end, { buffer = 0 })
+          end, "Rename")
 
           -- NOTE: word highlight, but using vim-illuminate instead...
           if client and client.server_capabilities.documentHighlightProvider then
