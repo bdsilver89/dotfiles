08bbc91a36d6eaec92011828d3496dfbd4b4d079
Brian Silver <github@briansilver.net>
2024-10-16 23:32:36 -0400

    update colorscheme and ui plugins

--- config/nvim-custom2/lua/plugins/ui.lua
+++ config/nvim-custom2/lua/plugins/ui.lua
@@ -27,4 +27,33 @@ return {
       end
     end,
   },
+
+  -- indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { show_start = false, show_end = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+    main = "ibl",
+  },
 }
