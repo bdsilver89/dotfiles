bdadb415bc6fb08d90aa6a56c1146fd1fad4e6ab
Brian Silver <github@briansilver.net>
2024-11-24 23:41:57 -0500

    add nvim markdown renderer

---
+++ config/nvim-custom/lua/plugins/markdown.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "norg", "rmd", "org" },
+    main = "render-markdown",
+    opts = {
+      heading = {
+        sign = false,
+      },
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+    },
+  },
+}
