eaefdc3869a3ae61b1feaed0c71597802b3653a1
Brian Silver <github@briansilver.net>
2024-11-28 12:34:27 -0500

    rework lsp and mason for nvim

--- config/nvim-custom/lua/plugins/lsp.lua
+++ config/nvim-custom/lua/plugins/lsp.lua
@@ -1,193 +1,112 @@
-local Notify = require("config.utils.notify")
-
--- LSP word navigation
-local W = {}
-
-W.defaults = {
-  enabled = true,
-  debounce = 200,
-  notify_jump = false,
-  notify_end = true,
-  foldopen = true,
-  jumplist = true,
-  modes = { "n", "i", "c" },
-}
-
-W.ns = vim.api.nvim_create_namespace("vim_lsp_references")
-W.timer = vim.uv.new_timer()
-
-function W.is_enabled(buf)
-  buf = buf or vim.api.nvim_get_current_buf()
-  local mode = vim.api.nvim_get_mode().mode:lower()
-  mode = mode:gsub("\22", "v"):gsub("\19", "s")
-  mode = mode:sub(1, 2) == "no" and "o" or mode
-  mode = mode:sub(1, 1):match("[ncitsvo]") or "n"
-  local clients = vim.lsp.get_clients({ bufnr = buf })
-  clients = vim.tbl_filter(function(client)
-    return client.supports_method("textDocument/documentHighlight", { bufnr = buf })
-  end, clients)
-  return W.defaults.enabled and vim.tbl_contains(W.defaults.modes, mode) and #clients > 0
-end
-
-function W.clear()
-  vim.lsp.buf.clear_references()
-end
-
-function W.update()
-  local buf = vim.api.nvim_get_current_buf()
-  W.timer:start(W.defaults.debounce, 0, function()
-    vim.schedule(function()
-      if vim.api.nvim_buf_is_valid(buf) then
-        vim.api.nvim_buf_call(buf, function()
-          if not W.is_enabled() then
-            return
-          end
-          vim.lsp.buf.document_highlight()
-          W.clear()
-        end)
-      end
-    end)
-  end)
-end
-
-function W.get()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local current, ret = nil, {}
-  for _, extmark in ipairs(vim.api.nvim_buf_get_extmarks(0, W.ns, 0, -1, { details = true })) do
-    local w = {
-      from = { extmark[2] + 1, extmark[3] },
-      to = { extmark[4].end_row + 1, extmark[4].end_col },
-    }
-    ret[#ret + 1] = w
-    if cursor[1] >= w.from[1] and cursor[1] <= w.to[1] and cursor[2] >= w.from[2] and cursor[2] <= w.to[2] then
-      current = #ret
-    end
-  end
-  return ret, current
-end
-
-function W.jump(count, cycle)
-  local words, idx = W.get()
-  if not idx then
-    return
-  end
-  idx = idx + count
-  if cycle then
-    idx = (idx - 1) % #words + 1
-  end
-  local target = words[idx]
-  if target then
-    if W.defaults.jumplist then
-      vim.cmd.normal({ "m`", bang = true })
-    end
-    vim.api.nvim_win_set_cursor(0, target.from)
-    if W.defaults.notify_jump then
-      Notify.info(("Reference [%d/%d]"):format(idx, #words), { title = "Words" })
-    end
-    if W.defaults.foldopen then
-      vim.cmd.normal({ "zv", bang = true })
-    end
-  elseif W.defaults.notify_end then
-    Notify.warn("No more references", { title = "Words" })
-  end
-end
-
-function W.setup()
-  local group = vim.api.nvim_create_augroup("lsp_words", { clear = true })
-
-  vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "ModeChanged" }, {
-    group = group,
-    callback = function()
-      if not W.is_enabled() then
-        W.clear()
-        return
-      end
-      if not ({ W.get() })[2] then
-        W.update()
-      end
-    end,
-  })
-end
-
-local function on_attach(_, buffer)
-  local function map(mode, lhs, rhs, desc)
-    vim.keymap.set(mode, lhs, rhs, { buffer = buffer, desc = "LSP: " .. desc })
-  end
-
-  -- NOTE: these assume fzf-lua for now...
-
-  map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-  map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-  map("n", "gr", vim.lsp.buf.references, "Show references")
-  map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-  map("n", "gy", vim.lsp.buf.type_definition, "Go to type definition")
-  map("n", "<leader>sh", vim.lsp.buf.signature_help, "Show signature help")
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "List workspace folders")
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
-  map("n", "<leader>cr", vim.lsp.buf.rename, "Rename")
-  map("n", "<leader>cl", "<cmd>LspInfo<cr>", "Info")
-
-  map("n", "<leader>cs", vim.lsp.buf.document_symbol, "symbols (document)")
-  map("n", "<leader>cS", vim.lsp.buf.workspace_symbol, "symbols (workspace)")
-
-  map("n", "<leader>q", function()
-    vim.diagnostic.setqflist({ open = false })
-    local win = vim.api.nvim_get_current_win()
-    vim.cmd.copen()
-    vim.api.nvim_set_current_win(win)
-  end, "set loclist diagnostics")
-
-  map("n", "]]", function()
-    W.jump(vim.v.count1)
-  end, "Next reference")
-  map("n", "[[", function()
-    W.jump(-vim.v.count1)
-  end, "Next reference")
-end
-
-local function on_init(client, _)
-  if client.supports_method("textDocument/semanticTokens") then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
-end
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { "markdown", "plaintext" },
-  snippetSupport = true,
-  preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
-  commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
-  resolveSupport = {
-    properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
-    },
-  },
-}
-
 return {
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "b0o/schemastore.nvim",
+    },
     -- NOTE: this cannot have a mason dependency for the current version of masoninstallall command
     -- that depends on this loading to configure itself first...
     config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("config_lspattach", { clear = true }),
+        callback = function(event)
+          local function map(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+
+          -- NOTE: these assume fzf-lua for now...
+
+          map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+          map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+          map("n", "gr", vim.lsp.buf.references, "Show references")
+          map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+          map("n", "gy", vim.lsp.buf.type_definition, "Go to type definition")
+          map("n", "<leader>sh", vim.lsp.buf.signature_help, "Show signature help")
+          map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
+          map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
+          -- map("n", "<leader>wl", function()
+          --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          -- end, "List workspace folders")
+          map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
+          map("n", "<leader>cr", vim.lsp.buf.rename, "Rename")
+
+          map("n", "<leader>cs", vim.lsp.buf.document_symbol, "symbols (document)")
+          map("n", "<leader>cS", vim.lsp.buf.workspace_symbol, "symbols (workspace)")
+
+          map("n", "<leader>cq", function()
+            vim.diagnostic.setqflist({ open = false })
+            local win = vim.api.nvim_get_current_win()
+            vim.cmd.copen()
+            vim.api.nvim_set_current_win(win)
+          end, "set loclist diagnostics")
+
+          map("n", "<leader>cl", function()
+            vim.diagnostic.setloclist({ open = false })
+            local win = vim.api.nvim_get_current_win()
+            vim.cmd.lopen()
+            vim.api.nvim_set_current_win(win)
+          end, "set loclist diagnostics")
+
+          -- map("n", "]]", function()
+          --   W.jump(vim.v.count1)
+          -- end, "Next reference")
+          -- map("n", "[[", function()
+          --   W.jump(-vim.v.count1)
+          -- end, "Next reference")
+
+          -- highlight word references in document
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.server_capabilities.documentHighlightProvider then
+            local highlight_group = vim.api.nvim_create_augroup("config_lsp_highlight", { clear = false })
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              group = highlight_group,
+              callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              group = highlight_group,
+              callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd("LspDetach", {
+              group = vim.api.nvim_create_augroup("config_lsp_highlight_detach", { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds({ group = "config_lsp_highlight", buffer = event2.buf })
+              end,
+            })
+          end
+
+          -- inlay hints
+          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+            map("n", "<leader>uh", function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+            end, "toggle inlay hints")
+          end
+        end,
+      })
+
       -- diagnostic config
       local x = vim.diagnostic.severity
 
+      local DiagnosticIcons = require("config.ui.icons.diagnostic")
       vim.diagnostic.config({
         virtual_text = { prefix = "" },
-        signs = { text = { [x.ERROR] = "󰅙", [x.WARN] = "", [x.INFO] = "󰋼", [x.HINT] = "󰌵" } },
+        signs = {
+          text = {
+            [x.ERROR] = DiagnosticIcons.Error,
+            [x.WARN] = DiagnosticIcons.Warning,
+            [x.INFO] = DiagnosticIcons.Info,
+            [x.HINT] = DiagnosticIcons.Hint,
+          },
+        },
         underline = true,
         float = { border = "single" },
       })
@@ -208,39 +127,66 @@ return {
         max_height = 7,
       })
 
-      W.setup()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-      local servers = { "bashls", "clangd", "jdtls", "jsonls", "neocmake", "ruff", "tailwindcss", "vtsls" }
-
-      for _, lsp in ipairs(servers) do
-        require("lspconfig")[lsp].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-          on_init = on_init,
-        })
-      end
-
-      require("lspconfig").lua_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        on_init = on_init,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-            workspace = {
-              library = {
-                vim.fn.expand("$VIMRUNTIME/lua"),
-                vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-                vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
-                vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
-                "${3rd}/luv/library",
-              },
-              maxPreload = 100000,
-              preloadFileSize = 10000,
+      local servers = {
+        bashls = {},
+        clangd = {},
+        jsonls = {
+          settings = {
+            json = {
+              schema = require("schemastore").json.schemas(),
+              validate = { enable = true },
             },
           },
+        },
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                library = {
+                  vim.fn.expand("$VIMRUNTIME/lua"),
+                  vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+                  vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+                  "${3rd}/luv/library",
+                },
+              },
+            },
+          },
+        },
+        neocmake = {},
+        ruff = {},
+        vtsls = {},
+      }
+
+      require("mason").setup()
+
+      -- additional tools
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, {
+        -- bash
+        "shfmt",
+
+        -- lua
+        "stylua",
+
+        -- python
+        "black",
+      })
+
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+      require("mason-lspconfig").setup({
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            require("lspconfig")[server_name].setup(server)
+          end,
         },
       })
     end,
