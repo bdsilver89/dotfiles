66393642922cb24059ab2de289523863413f412e
Brian Silver <github@briansilver.net>
2024-11-28 11:35:05 -0500

    fix git browse settings for gitlab

--- config/nvim-custom/lua/config/utils/git.lua
+++ config/nvim-custom/lua/config/utils/git.lua
@@ -22,13 +22,20 @@ local defaults = {
     { "%.git$", "" },
   },
   url_patterns = {
-    ["github.com"] = {
+    ["github%.com"] = {
       branch = "/tree/{branch}",
       file = "/blob/{branch}/{file}#L{line}",
+      commit = "/commit/{commit}",
     },
-    ["gitlab.com"] = {
+    [".*gitlab.*%.com"] = { -- NOTE: wildcard here to help with various corporate gitlab providers
       branch = "/-/tree/{branch}",
       file = "/-/blob/{branch}/{file}#L{line}",
+      commit = "/-/commit/{commit}",
+    },
+    ["bitbucket%.org"] = {
+      branch = "/src/{branch}",
+      file = "/src/{branch}/{file}#lines-{line}",
+      commit = "/commits/{commit}",
     },
   },
 }
@@ -42,11 +49,19 @@ local function system(cmd, err)
   return vim.split(vim.trim(proc), "\n")
 end
 
+local function is_valid_commit_hash(hash, cwd)
+  if not (hash:match("^[a-fA-F0-9]+$") and #hash >= 7) then
+    return false
+  end
+  system({ "git", "-C", cwd, "rev-parse", "--verify", hash }, "Invalid commit hash")
+  return true
+end
+
 local function get_repo(remote, opts)
   opts = vim.tbl_deep_extend("force", defaults, opts or {})
   local ret = remote
   for _, pattern in ipairs(opts.remote_patterns) do
-    ret = ret:gsub(pattern[1], pattern[2])
+    ret = ret:gsub(pattern[1], pattern[2]) --[[@as string]]
   end
   return ret:find("https://") == 1 and ret or ("https://%s"):format(ret)
 end
@@ -54,6 +69,7 @@ end
 local function get_url(repo, opts)
   opts = vim.tbl_deep_extend("force", defaults, opts or {})
   for remote, patterns in pairs(opts.url_patterns) do
+    Notify.info("repo=" .. repo .. ", remote=" .. remote)
     if repo:find(remote) then
       return patterns[opts.what] and (repo .. patterns[opts.what]) or repo
     end
@@ -61,12 +77,29 @@ local function get_url(repo, opts)
   return repo
 end
 
+local git_cache = {}
+local function is_git_root(dir)
+  if git_cache[dir] == nil then
+    git_cache[dir] = vim.uv.fs_stat(dir .. "/.git") ~= nil
+  end
+  return git_cache[dir]
+end
+
 function M.get_root(path)
   path = path or 0
   path = type(path) == "number" and vim.api.nvim_buf_get_name(path) or path
   path = vim.fs.normalize(path)
-  local git_root = vim.fs.find(".git", { path = path, upward = true })[1]
-  return git_root and vim.fn.fnamemodify(git_root, ":h") or nil
+  path = path == "" and vim.uv.cwd() .. "/foo" or path
+  for dir in vim.fs.parents(path) do
+    if git_cache[dir] then
+      return vim.fs.normalize(dir) or nil
+    end
+  end
+  for dir in vim.fs.parents(path) do
+    if is_git_root(dir) then
+      return vim.fs.normalize(dir) or nil
+    end
+  end
 end
 
 function M.blame_line(opts)
@@ -96,13 +129,29 @@ function M.browse(opts)
   local file = vim.api.nvim_buf_get_name(0)
   file = file and (vim.uv.fs_stat(file) or {}).type == "file" and vim.fs.normalize(file) or nil
   local cwd = file and vim.fn.fnamemodify(file, ":h") or vim.fn.getcwd()
+  local word = vim.fn.expand("<cword>")
+  local is_commit = is_valid_commit_hash(word, cwd)
   local fields = {
     branch = system({ "git", "-C", cwd, "rev-parse", "--abbrev-ref", "HEAD" }, "Failed to get current branch")[1],
     file = file and system({ "git", "-C", cwd, "ls-files", "--full-name", file }, "Failed to get git file path")[1],
     line = file and vim.fn.line("."),
+    commit = is_commit and word,
   }
-  opts.what = opts.what == "file" and not fields.file and "branch" or opts.what
-  opts.what = opts.what == "branch" and not fields.branch and "repo" or opts.what
+
+  if vim.fn.mode() == "v" or vim.fn.mode() == "V" then
+    local start_line = vim.fn.line("v")
+    local end_line = vim.fn.line(".")
+    if start_line > end_line then
+      start_line, end_line = end_line, start_line
+    end
+    fields.line = file and start_line .. "-L" .. end_line
+  else
+    fields.line = file and vim.fn.line(".")
+  end
+
+  opts.what = is_commit and "commit" or opts.what == "commit" and not fields.commit and "file" or opts.what
+  opts.what = not is_commit and opts.what == "file" and not fields.file and "branch" or opts.what
+  opts.what = not is_commit and opts.what == "branch" and not fields.branch and "repo" or opts.what
 
   local remotes = {}
 
