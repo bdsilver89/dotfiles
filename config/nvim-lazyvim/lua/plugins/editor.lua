466461c015f1b1f8a51f093c1cd91562de005678
Brian Silver <github@briansilver.net>
2024-12-06 13:18:56 -0500

    clean up lazyvim and add overseer

--- config/nvim-lazyvim/lua/plugins/editor.lua
+++ config/nvim-lazyvim/lua/plugins/editor.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  {
     "alexghergh/nvim-tmux-navigation",
     keys = {
       { "<c-h>", "<cmd>NvimTmuxNavigateLeft<cr>" },
@@ -8,5 +15,56 @@ return {
       { "<c-l>", "<cmd>NvimTmuxNavigateRight<cr>" },
     },
     opts = {},
+  },
+
+  {
+    "stevearc/oil.nvim",
+    keys = {
+      {
+        "-",
+        function()
+          require("oil").open()
+        end,
+        desc = "Explorer Oil (Root Dir)",
+      },
+      {
+        "_",
+        function()
+          require("oil").open(vim.uv.cwd())
+        end,
+        desc = "Explorer Oil (cwd)",
+      },
+    },
+    opts = {},
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+          prompt_position = "top",
+        },
+      },
+    },
+  },
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gn", "<cmd>Neogit<cr>", desc = "Status" },
+    },
+    opts = {
+      integrations = {
+        diffview = true,
+      },
+    },
   },
 }
