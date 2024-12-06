e6d22d67157198a14846645ac694d7da11fae9a0
Brian Silver <github@briansilver.net>
2024-12-06 12:14:33 -0500

    lazyvim

--- config/nvim-lazyvim/lua/plugins/oil.lua
+++ config/nvim-lazyvim/lua/plugins/oil.lua
@@ -3,30 +3,18 @@ return {
     "stevearc/oil.nvim",
     keys = {
       {
-        "<leader>fo",
+        "-",
         function()
           require("oil").open()
         end,
-        desc = "Explorer Oil (Directory of Current File)",
+        desc = "Explorer Oil (Root Dir)",
       },
       {
-        "<leader>fO",
+        "_",
         function()
           require("oil").open(vim.uv.cwd())
         end,
         desc = "Explorer Oil (cwd)",
-      },
-      {
-        "<leader>o",
-        "<leader>fo",
-        desc = "Explorer Oil (Directory of Current File)",
-        remap = true,
-      },
-      {
-        "<leader>O",
-        "<leader>fO",
-        desc = "Explorer Oil (cwd)",
-        remap = true,
       },
     },
     opts = {},
