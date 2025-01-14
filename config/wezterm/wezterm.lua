b48ff178b54dd5d34de44cb2ecbef9503622c00e
Brian Silver <github@briansilver.net>
2025-01-13 21:30:16 -0500

    added wezterm config for nvim navigator support

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -45,6 +45,35 @@ wezterm.on("toggle-colorscheme", function(window, pane)
 	window:set_config_overrides(overrides)
 end)
 
+local function isViProcess(pane)
+	return pane:get_foreground_process_name():find("n?vim") ~= nil or pane:get_title():find("n?vim") ~= nil
+end
+
+local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+	if isViProcess(pane) then
+		window:perform_action(
+			-- This should match the keybinds you set in Neovim.
+			act.SendKey({ key = vim_direction, mods = "CTRL" }),
+			pane
+		)
+	else
+		window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+	end
+end
+
+wezterm.on("ActivatePaneDirection-right", function(window, pane)
+	conditionalActivatePane(window, pane, "Right", "l")
+end)
+wezterm.on("ActivatePaneDirection-left", function(window, pane)
+	conditionalActivatePane(window, pane, "Left", "h")
+end)
+wezterm.on("ActivatePaneDirection-up", function(window, pane)
+	conditionalActivatePane(window, pane, "Up", "k")
+end)
+wezterm.on("ActivatePaneDirection-down", function(window, pane)
+	conditionalActivatePane(window, pane, "Down", "j")
+end)
+
 return {
 	animation_fps = 1,
 	color_scheme = "Catppuccin Mocha",
@@ -59,6 +88,10 @@ return {
 	window_background_opacity = 1,
 	-- window_decorations = "RESIZE",
 	keys = {
+		{ key = "h", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-left") },
+		{ key = "j", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-down") },
+		{ key = "k", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-up") },
+		{ key = "l", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-right") },
 		{
 			key = "e",
 			mods = "CTRL|SHIFT",
@@ -93,25 +126,5 @@ return {
 				domain = "CurrentPaneDomain",
 			}),
 		},
-		-- {
-		-- 	key = "h",
-		-- 	mods = "CTRL|SHIFT",
-		-- 	action = act.ActivatePaneDirection("Left"),
-		-- },
-		-- {
-		-- 	key = "j",
-		-- 	mods = "CTRL|SHIFT",
-		-- 	action = act.ActivatePaneDirection("Down"),
-		-- },
-		-- {
-		-- 	key = "k",
-		-- 	mods = "CTRL|SHIFT",
-		-- 	action = act.ActivatePaneDirection("Up"),
-		-- },
-		-- {
-		-- 	key = "l",
-		-- 	mods = "CTRL|SHIFT",
-		-- 	action = act.ActivatePaneDirection("Right"),
-		-- },
 	},
 }
