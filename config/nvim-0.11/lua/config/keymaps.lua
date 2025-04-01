8922a0944c2346be66c245b70c36c352d92f8020
Brian Silver <github@briansilver.net>
2025-03-31 20:54:21 -0400

    nvim terminal setup

--- config/nvim-0.11/lua/config/keymaps.lua
+++ config/nvim-0.11/lua/config/keymaps.lua
@@ -17,6 +17,20 @@ map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = tru
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
+-- resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- move lines
+map("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
 map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
@@ -45,6 +59,10 @@ map("n", "<esc>", "<cmd>nohlsearch<cr>")
 
 -- terminal
 map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Exit terminal mode" })
+map("t", "<c-h>", "<cmd>wincmd h<cr>")
+map("t", "<c-j>", "<cmd>wincmd j<cr>")
+map("t", "<c-k>", "<cmd>wincmd k<cr>")
+map("t", "<c-l>", "<cmd>wincmd l<cr>")
 
 -- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
@@ -59,11 +77,11 @@ map("n", "grl", vim.diagnostic.setloclist, { desc = "vim.diagnostic.setloclist()
 
 -- popups/completion
 -- stylua: ignore start
-map("i", "<tab>", function() return vim.fn.pumvisible() == 1 and "<c-n>" or "<tab>" end, { expr = true })
-map("i", "<s-tab>", function() return vim.fn.pumvisible() == 1 and "<c-p>" or "<s-tab>" end, { expr = true })
-map("i", "<down>", function() return vim.fn.pumvisible() == 1 and "<c-n>" or "<down>" end, { expr = true })
-map("i", "<up>", function() return vim.fn.pumvisible() == 1 and "<c-p>" or "<up>" end, { expr = true })
-map("i", "<cr>", function() return vim.fn.pumvisible() == 1 and "<c-y>" or "<cr>" end, { expr = true })
+-- map("i", "<tab>", function() return vim.fn.pumvisible() == 1 and "<c-n>" or "<tab>" end, { expr = true })
+-- map("i", "<s-tab>", function() return vim.fn.pumvisible() == 1 and "<c-p>" or "<s-tab>" end, { expr = true })
+-- map("i", "<down>", function() return vim.fn.pumvisible() == 1 and "<c-n>" or "<down>" end, { expr = true })
+-- map("i", "<up>", function() return vim.fn.pumvisible() == 1 and "<c-p>" or "<up>" end, { expr = true })
+-- map("i", "<cr>", function() return vim.fn.pumvisible() == 1 and "<c-y>" or "<cr>" end, { expr = true })
 
 map("i", "<c-[>", "<c-x><c-[>")
 map("i", "<c-f>", "<c-x><c-f>")
