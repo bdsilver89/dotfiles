54ed24bebaf0c3591d7567185bc790d72144e600
Brian Silver <github@briansilver.net>
2025-04-20 19:31:25 -0400

    fix(bash) error in git ps1

--- bashrc.d/10_prompt.sh
+++ bashrc.d/10_prompt.sh
@@ -21,4 +21,5 @@ __prompt() {
   echo "\$ $NOCOL"
 }
 
-PS1="$(__username)$(__separator @ "34")$(__host)$(__separator : "34")$(__path)$(__git_ps1)$(__prompt)"
+# PS1="$(__username)$(__separator @ "34")$(__host)$(__separator : "34")$(__path)$(__git_ps1)$(__prompt)"
+PS1='\[\e[0;34m\]\u@\h\[\e[0m\]:\[\e[0;32m\]\w\[\e[0;33m\]$(__git_ps1 " (%s)")\[\e[0m\]\$ '
