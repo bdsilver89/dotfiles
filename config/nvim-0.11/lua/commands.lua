9ac94981575507d098f936e9523031d9f9ae3bf5
Brian Silver <github@briansilver.net>
2025-05-06 11:01:39 -0400

    feat(nvim): add format user command

--- config/nvim-0.11/lua/commands.lua
+++ config/nvim-0.11/lua/commands.lua
@@ -7,6 +7,18 @@ vim.api.nvim_create_user_command("ToggleFormat", function()
   )
 end, { desc = "Toggle conform.nvim auto-formatting", nargs = 0 })
 
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
+
 vim.api.nvim_create_user_command("LspRestart", function()
   vim.lsp.stop_client(vim.lsp.get_clients())
   vim.cmd("edit")
