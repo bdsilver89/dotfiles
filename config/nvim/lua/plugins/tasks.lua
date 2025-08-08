9a2993c59e6109ac85c9650dc443926469b17661
Brian Silver <github@briansilver.net>
2025-08-08 11:16:45 -0400

    feat(nvim): async grep via overseer

--- config/nvim/lua/plugins/tasks.lua
+++ config/nvim/lua/plugins/tasks.lua
@@ -185,6 +185,30 @@ end, {
   bang = true,
 })
 
+vim.api.nvim_create_user_command("Grep", function(params)
+  -- Insert args at the '$*' in the grepprg
+  local cmd, num_subs = vim.o.grepprg:gsub("%$%*", params.args)
+  if num_subs == 0 then
+    cmd = cmd .. " " .. params.args
+  end
+  local task = require("overseer").new_task({
+    cmd = vim.fn.expandcmd(cmd),
+    components = {
+      {
+        "on_output_quickfix",
+        errorformat = vim.o.grepformat,
+        open = not params.bang,
+        open_height = 8,
+        items_only = true,
+      },
+      -- We don't care to keep this around as long as most tasks
+      { "on_complete_dispose", timeout = 30 },
+      "default",
+    },
+  })
+  task:start()
+end, { nargs = "*", bang = true, complete = "file" })
+
 -- CMake user commands
 vim.api.nvim_create_user_command("CMakeConfigure", function(params)
   local build_dir = get_cmake_build_dir()
