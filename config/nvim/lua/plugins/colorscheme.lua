86985247fee69dbbe120e32d0b67848b5fc82b4b
Brian Silver <github@briansilver.net>
2025-08-29 10:41:54 -0400

    fix(nvim): move mini.nvim repo, overseer integration

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -14,6 +14,10 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    opts = {},
+    opts = {
+      integrations = {
+        overseer = true,
+      },
+    },
   },
 }
