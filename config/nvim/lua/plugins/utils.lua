5161ed0668a88dd659ac139b89a6b1532eaa7f74
Brian Silver <github@briansilver.net>
2025-12-18 15:44:57 -0500

    nvim to lazyvim

---
+++ config/nvim/lua/plugins/utils.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Oil file explorer" },
+    },
+    opts = {},
+  },
+
+  {
+    "NMAC427/guess-indent.nvim",
+    event = "LazyFile",
+    cmd = "GuessIndent",
+    opts = {},
+  },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateRight",
+      "TmuxNavigateUp",
+      "TmuxNavigateDown",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to left pane" },
+      { "<c-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to lower pane" },
+      { "<c-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to upper pane" },
+      { "<c-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to right pane" },
+      { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Go to previous pane" },
+    },
+  },
+}
