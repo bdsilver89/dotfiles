ce06c815c69b0e575ddb6c15ea4dd718f7059f25
Brian Silver <github@briansilver.net>
2026-04-06 20:02:24 -0400

    clean up nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,72 +1,87 @@
--- Globals ====================================================================
+vim.pack.add({
+  "https://github.com/tpope/vim-sleuth",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/mason-org/mason.nvim",
+  "https://github.com/mason-org/mason-lspconfig.nvim",
+  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/ibhagwan/fzf-lua",
+  "https://github.com/lewis6991/gitsigns.nvim",
+})
 
 vim.g.mapleader = " "
-vim.g.maplolcaleader = " "
 
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_node_provider = 0
+vim.o.autocomplete = true
+vim.o.autoread = true
+vim.o.breakindent = true
+vim.o.confirm = true
+vim.o.cursorline = true
+vim.o.foldexpr = "v:vim.treesitter.foldexpr()"
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.ignorecase = true
+vim.o.laststatus = 3
+vim.o.list = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 10
+vim.o.signcolumn = "yes"
+vim.o.smartcase = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.timeoutlen = 300
+vim.o.undofile = true
+vim.o.updatetime = 250
+vim.o.wrap = false
+vim.opt.completeopt = { "menuone", "noselect", "popup", "fuzzy" }
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Options ====================================================================
+vim.schedule(function() vim.o.clipboard = "unnamedplus" end)
+vim.cmd.colorscheme("catppuccin")
 
-vim.opt.autocomplete = true
-vim.opt.autoread = true
-vim.opt.breakindent = true
-vim.opt.completeopt = { "menuone", "noselect", "popup", "fuzzy", "nearest" }
-vim.opt.confirm = true
-vim.opt.cursorline = true
-vim.opt.diffopt:append("linematch:60")
-vim.opt.expandtab = true
-vim.opt.fillchars = { eob = " " }
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldlevel = 99
-vim.opt.foldmethod = "expr"
-vim.opt.ignorecase = true
-vim.opt.indentexpr = "v:lua.vim.treesitter.indentexpr()"
-vim.opt.laststatus = 3
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣", }
-vim.opt.number = true
-vim.opt.pumheight = 10
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 2
-vim.opt.shortmess:append("cw")
-vim.opt.signcolumn = "yes"
-vim.opt.smartcase = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.tabstop = 2
-vim.opt.timeoutlen = 300
-vim.opt.undofile = true
-vim.opt.undolevels = 10000
-vim.opt.updatetime = 250
-vim.opt.wrap = false
-
-vim.schedule(function()
-  vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
-  vim.cmd.colorscheme("catppuccin")
-end)
-
-vim.opt.statusline = table.concat({
-  "%<%f %h%w%m%r",
-  " %{% v:lua.require('vim._core.util').term_exitcode() %}",
-  " %{% exists('b:gitsigns_head') ? ' '..b:gitsigns_head : '' %}",
-  "%{% exists('b:gitsigns_status') && b:gitsigns_status != '' ? ' '..b:gitsigns_status : '' %}",
-  "%=",
-  "%{% luaeval('(package.loaded[''vim.lsp''] and vim.lsp.status()) or '''' ')%} ",
-  "%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}",
-  "%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}",
-  "%{% &busy > 0 ? '◐ ' : '' %}",
-  "%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}",
-  "%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}",
+vim.diagnostic.config({
+  severity_sort = true,
+  underline = true,
+  update_in_insert = false,
+  virtual_lines = {
+    curent_line = true,
+  },
+  virtual_text = {
+    curent_line = false,
+  },
 })
 
-require("vim._core.ui2").enable({
-  enable = true,
+local lsp_servers = {
+  basedpyright = {},
+  clangd = { mason = false },
+  jdtls = {},
+  lua_ls = {
+    settings = {
+      Lua = { workspace = { library = vim.api.nvim_get_runtime_file("lua", true) } },
+    },
+  },
+  rust_analyzer = { mason = false },
+}
+
+require("mason").setup()
+require("oil").setup()
+require("fzf-lua").setup()
+
+local mlsp_servers = {}
+for server, opts in pairs(lsp_servers) do
+  local use_mason = opts and opts.mason ~= false
+  opts.mason = nil
+  vim.lsp.config(server, opts)
+  vim.lsp.enable(server)
+  if use_mason then
+    mlsp_servers[#mlsp_servers + 1] = server
+  end
+end
+require("mason-lspconfig").setup({
+  ensure_installed = mlsp_servers,
 })
 
--- Keymaps ====================================================================
+require("gitsigns").setup()
 
 -- stylua: ignore start
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
@@ -85,29 +100,44 @@ vim.keymap.set("i", ",", ",<c-g>u")
 vim.keymap.set("i", ".", ".<c-g>u")
 vim.keymap.set("i", ";", ";<c-g>u")
 
-
 vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr>")
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>")
 vim.keymap.set("n", "<leader>-", "<c-w>s")
 vim.keymap.set("n", "<leader>|", "<c-w>v")
+
 vim.keymap.set("n", "<leader>ud", function()
   vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end, { desc = "Toggle Diagnostics" })
 vim.keymap.set("n", "<leader>uw", function()
   vim.o.wrap = not vim.o.wrap
 end, { desc = "Toggle Wrap" })
+
 vim.keymap.set("n", "<leader>ut", function()
   vim.cmd.packadd("nvim.undotree")
   require("undotree").open()
 end, { desc = "Undotree" })
+
 vim.keymap.set("n", "<leader>xl", function()
   if vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 then vim.cmd.lclose() else vim.cmd.lopen() end
 end, { desc = "Location List" })
+
 vim.keymap.set("n", "<leader>xq", function()
   if vim.fn.getqflist({ winid = 0 }).winid ~= 0 then vim.cmd.cclose() else vim.cmd.copen() end
 end, { desc = "Quickfix List" })
+
+local function tmux_nav(vdir, tdir)
+  local win = vim.api.nvim_get_current_win()
+  vim.cmd.wincmd(vdir)
+  if (vim.env.TMUX ~= nil) and vim.api.nvim_get_current_win() == win then
+    vim.system({ "tmux", "select-pane", "-" .. tdir })
+  end
+end
+vim.keymap.set({ "n", "t" }, "<c-h>", function() tmux_nav("h", "L") end)
+vim.keymap.set({ "n", "t" }, "<c-j>", function() tmux_nav("j", "D") end)
+vim.keymap.set({ "n", "t" }, "<c-k>", function() tmux_nav("k", "U") end)
+vim.keymap.set({ "n", "t" }, "<c-l>", function() tmux_nav("l", "R") end)
 
 vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Oil" })
 
@@ -117,17 +147,15 @@ vim.keymap.set("n", "<leader>,", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" }
 vim.keymap.set("n", "<leader>/", "<cmd>FzfLua live_grep<cr>", { desc = "Grep" })
 -- stylua: ignore end
 
-
--- Autocmds ===================================================================
 local group = vim.api.nvim_create_augroup("config", { clear = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight on text yank",
   group = group,
   callback = function()
     vim.hl.on_yank()
   end,
 })
+
 vim.api.nvim_create_autocmd("VimResized", {
   desc = "Resize splits",
   group = group,
@@ -137,6 +165,7 @@ vim.api.nvim_create_autocmd("VimResized", {
     vim.cmd("tabnext " .. current_tab)
   end,
 })
+
 vim.api.nvim_create_autocmd("FileType", {
   desc = "Close with <q>",
   pattern = { "nvim-undotree", "git", "help", "man", "qf" },
@@ -147,8 +176,8 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end,
 })
+
 vim.api.nvim_create_autocmd("FileType", {
-  desc = "Install treesitter",
   group = group,
   callback = function(ev)
     if vim.bo[ev.buf].buftype ~= "" then
@@ -158,33 +187,26 @@ vim.api.nvim_create_autocmd("FileType", {
     if lang and require("nvim-treesitter.parsers")[lang] then
       pcall(require("nvim-treesitter").install, { lang })
     end
+    pcall(vim.treesitter.start)
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
+
 vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "LSP setup",
   group = group,
   callback = function(ev)
-    local buf = ev.buf
     local client_id = ev.data.client_id
     local client = vim.lsp.get_client_by_id(client_id)
     if not client then
       return
     end
 
-    local map = function(lhs, rhs, opts, mode, requires)
-      if requires and not client:supports_method(requires) then
-        return
-      end
-      mode = mode or "n"
-      opts = type(opts) == "string" and { desc = opts } or opts
-      opts.buffer = buf
-      vim.keymap.set(mode, lhs, rhs, opts)
+    local map = function(lhs, rhs, desc)
+      vim.keymap.set("n", lhs, rhs, { buffer = ev.buf, desc = desc })
     end
 
     map("grd", vim.lsp.buf.definition, "vim.lsp.buf.definition()")
     map("grD", vim.lsp.buf.declaration, "vim.lsp.buf.declaration()")
-    map("grf", vim.lsp.buf.format, "vim.lsp.buf.format()")
-    map("gro", vim.diagnostic.open_float, "vim.diagnotic.open_float()")
 
     if client:supports_method("textDocument/completion") then
       vim.lsp.completion.enable(true, client_id, ev.buf, { autotrigger = true })
@@ -193,7 +215,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.lsp.inline_completion.enable(true, { bufnr = buf })
     end
     if client:supports_method("textDocument/documentHighlight") then
-      local hl_group = vim.api.nvim_create_augroup("lsp_document_highlight_" .. buf, { clear = true })
+      local hl_group = vim.api.nvim_create_augroup("lsp_document_highlight_" .. ev.buf, { clear = true })
       vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
         buffer = buf,
         group = hl_group,
@@ -227,93 +249,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
   end,
 })
+
 vim.api.nvim_create_autocmd("LspProgress", {
   desc = "Redraw statusline on LSP progress",
   callback = function()
     vim.cmd.redrawstatus()
-  end,
-})
-
--- LSP ========================================================================
-
-local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
-  :map(function(path)
-    return vim.fn.fnamemodify(path, ":t:r")
-  end)
-  :totable()
-vim.lsp.enable(servers)
-
-vim.diagnostic.config({
-  severity_sort = true,
-  update_in_insert = false,
-  underline = true,
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN] = " ",
-      [vim.diagnostic.severity.INFO] = " ",
-      [vim.diagnostic.severity.HINT] = " ",
-    },
-  },
-  virtual_text = {
-    current_line = false,
-  },
-  virtual_lines = {
-    current_line = true,
-  },
-})
-
--- Plugins ====================================================================
-vim.pack.add({
-  "https://github.com/tpope/vim-sleuth",
-  "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/mason-org/mason.nvim",
-  "https://github.com/stevearc/oil.nvim",
-  "https://github.com/ibhagwan/fzf-lua",
-  "https://github.com/lewis6991/gitsigns.nvim",
-})
-vim.cmd.packadd("nvim.difftool")
-
-require("mason").setup()
-require("oil").setup()
-
-require("gitsigns").setup({
-  current_line_blame = true,
-  on_attach = function(buf)
-    local gs = require("gitsigns")
-
-    local map = function(mode, lhs, rhs, desc)
-      vim.keymap.set(mode, lhs, rhs, { buffer = buf, desc = desc })
-    end
-
-    -- stylua: ignore start
-    local nav = function(key, dir)
-      if vim.wo.diff then
-        vim.cmd.normal(key, { bang = true })
-      else
-        gs.nav_hunk(dir)
-      end
-    end
-
-    map("n", "]c", function() nav("]c", "next") end, "Next Hunk")
-    map("n", "[c", function() nav("[c", "prev") end, "Prev Hunk")
-    map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage Hunk")
-    map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset Hunk")
-
-    map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
-    map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
-    map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
-    map("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
-    map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
-    map("n", "<leader>hi", gs.preview_hunk_inline, "Preview Hunk Inline")
-    map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame Line")
-    map("n", "<leader>hd", gs.diffthis, "Diff Against Index")
-    map("n", "<leader>hD", function() gs.diffthis("@") end, "Diff Agsint HEAD")
-    map("n", "<leader>hQ", function() gs.setqflist("all") end, "Send to Quickfix")
-
-    map("n", "<leader>ub", gs.toggle_current_line_blame, "Toggle Blame Line")
-
-    map({ "o", "x" }, "ih", gs.select_hunk, "Select Hunk")
-    -- stylua: ignore end
   end,
 })
