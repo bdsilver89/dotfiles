8558635cfdb220aae5ed89dced3800c825b0c825
Brian Silver <github@briansilver.net>
2026-09-05 19:22:26 -0400

    nvim config fix fzf and add hipatterns

--- home/dot_config/nvim/plugin/fzf-lua.lua
+++ home/dot_config/nvim/plugin/fzf-lua.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
   "https://github.com/ibhagwan/fzf-lua",
-}, { load = false })
+})
 
 vim.schedule(function()
   local fzf = require("fzf-lua")
--- home/dot_config/nvim/plugin/mini.lua
+++ home/dot_config/nvim/plugin/mini.lua
@@ -10,5 +10,21 @@ vim.schedule(function()
 
   require("mini.pairs").setup({})
   require("mini.surround").setup({})
-  require("mini.indentscope").setup({})
+
+  local indentscope = require("mini.indentscope")
+  indentscope.setup({
+    draw = {
+      animation = indentscope.gen_animation.none()
+    }
+  })
+
+  local hipatterns = require("mini.hipatterns")
+  hipatterns.setup({
+    highlighters = {
+      fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+      hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+      todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+      note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+    }
+  })
 end)
