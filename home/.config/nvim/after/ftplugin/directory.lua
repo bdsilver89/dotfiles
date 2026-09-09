e26afad46ad5855da7b2a0c5fda56dfb6a894bb7
Brian Silver <github@briansilver.net>
2026-09-08 22:50:50 -0400

    nvim patch directory icons

--- home/.config/nvim/after/ftplugin/directory.lua
+++ home/.config/nvim/after/ftplugin/directory.lua
@@ -2,22 +2,24 @@ vim.opt_local.bufhidden = "wipe"
 vim.keymap.set("n", "q", "<cmd>bd<CR>", { buffer = true, silent = true })
 
 local ns = vim.api.nvim_create_namespace("dir_icons")
-local devicons = require("nvim-web-devicons")
+local has_devicons, devicons = pcall(require, "nvim-web-devicons")
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "DirReadPost",
-  group = vim.api.nvim_create_augroup("dir_icons", { clear = true }),
-  callback = function(ev)
-    vim.api.nvim_buf_clear_namespace(ev.buf, ns, 0, -1)
-    for i, line in ipairs(vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)) do
-      local icon, hl = " ", "Directory"
-      if line:sub(-1) ~= "/" then
-        icon, hl = devicons.get_icon(line, nil, { default = true })
+if has_devicons then
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "DirReadPost",
+    group = vim.api.nvim_create_augroup("dir_icons", { clear = true }),
+    callback = function(ev)
+      vim.api.nvim_buf_clear_namespace(ev.buf, ns, 0, -1)
+      for i, line in ipairs(vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)) do
+        local icon, hl = " ", "Directory"
+        if line:sub(-1) ~= "/" then
+          icon, hl = devicons.get_icon(line, nil, { default = true })
+        end
+        vim.api.nvim_buf_set_extmark(ev.buf, ns, i - 1, 0, {
+          virt_text = { { icon .. " ", hl } },
+          virt_text_pos = "inline",
+        })
       end
-      vim.api.nvim_buf_set_extmark(ev.buf, ns, i - 1, 0, {
-        virt_text = { { icon .. " ", hl } },
-        virt_text_pos = "inline",
-      })
-    end
-  end,
-})
+    end,
+  })
+end
