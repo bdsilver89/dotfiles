8582a09dbd94bec608873e95df89b5d346f9b3e2
Brian Silver <github@briansilver.net>
2026-09-18 08:41:44 -0400

    remove .vim folder from install script

--- install.sh
+++ install.sh
@@ -675,7 +675,6 @@ build_link_map() {
     add_files ".config/tuicr"
     add_files ".config/opencode"
     add_files ".pi"
-    add_files ".vim"
 
     # .config itself is shared, so single files in it are linked individually.
     add_link ".config/nvim"
