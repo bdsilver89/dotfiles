63ea4803d33fbf5abd0dcb4113eef3d82f8d08d7
Brian Silver <github@briansilver.net>
2026-09-18 10:23:59 -0400

    cleanup nvim plugins

--- home/.config/nvim/plugin/colorscheme.lua
+++ home/.config/nvim/plugin/colorscheme.lua
@@ -1,12 +1,12 @@
 vim.pack.add({
-  { src = "https://github.com/Everblush/nvim", name = "everblush" },
+  -- { src = "https://github.com/Everblush/nvim", name = "everblush" },
   "https://github.com/neanias/everforest-nvim",
-  "http://github.com/ellisonleao/gruvbox.nvim",
+  -- "http://github.com/ellisonleao/gruvbox.nvim",
 })
 
-require("gruvbox").setup()
+-- require("gruvbox").setup()
 require("everforest").setup({})
 
 -- vim.cmd.colorscheme("everblush")
--- vim.cmd.colorscheme("everforest")
-vim.cmd.colorscheme("gruvbox")
+vim.cmd.colorscheme("everforest")
+-- vim.cmd.colorscheme("gruvbox")
